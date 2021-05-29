312ef5e29492187cc2902359fbcb057228b46768
ashwinexe <53075480+ashwinexe@users.noreply.github.com>
2021-05-29 12:59:42 +0530

    tried EaslJS library

--- duck/lib/board.js
+++ duck/lib/board.js
@@ -2,5 +2,28 @@ class Board {
     constructor (game) {
         this.game = game;
         this.stage = game.stage
+        this.stage = game.stage
+        this.canvas = game.canvas;
+        this.score = 0;
+        this.scoreContainer = new createjs.Container();
+        this.miniDucksContainer = new createjs.Container();
+
+
+        this.round = 1;
+        this.createBackground();
+        this.renderMiniDucks();
+        this.flashMiniDucks = this.flashMiniDucks.bind(this);
+        this.stopMiniDuckFlash = this.stopMiniDuckFlash.bind(this);
     }
+
+    createBackground() {
+    const background = new createjs.Bitmap('./assets/images/background.png');
+    background.scaleX = 2.5;
+    background.scaleY = 2;
+    this.stage.addChild(background);
+    this.stage.addChild(this.roundContainer)
+    new Round(this);
+  }
+
+
 }
