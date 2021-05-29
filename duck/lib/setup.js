987f3f9f9f850015e566e3e4be7abfddfda48f82
ashwinexe <53075480+ashwinexe@users.noreply.github.com>
2021-05-29 16:22:50 +0530

    Update setup.js

--- duck/lib/setup.js
+++ duck/lib/setup.js
@@ -1,3 +1,14 @@
+//Aliases
+let Application = PIXI.Application,
+    Container = PIXI.Container,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Graphics = PIXI.Graphics,
+    TextureCache = PIXI.utils.TextureCache,
+    Sprite = PIXI.Sprite,
+    Text = PIXI.Text,
+    TextStyle = PIXI.TextStyle;
+
 function setup {
 
     // make the game scene and add it to stage
@@ -16,8 +27,33 @@ function setup {
     speed = 1,
     direction = 1;
 
+    const texture_duck = PIXI.Texture.from('assets/images/duck.svg');
+
     // an array to store all ducks
     ducks = [];
+
+    //make duck
+    let duck = new Sprite(texture_duck);
+
+    //xOffset is point of first duck from left of screen
+    // ket x = spacing * i  + xOffset;
+
+    // give duck random x position
+    let x = randomInt(0, app.stage.width = duck.width);
+
+    // set duck's position
+    duck.x = x;
+    duck.y = app.renderer.height;
+
+    duck.vy = speed * direction;
+
+    //change direction for next blob
+    direction *= -1;
+
+    //pushing ducks in array
+    ducks.push(duck);
+
+    gameScene.addChild(duck);
 
     app.ticker.add(delta => gameLoop(delta));
 }
